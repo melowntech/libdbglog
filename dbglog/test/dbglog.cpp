1e3e0642348d0237b678f3027a4236849b7828fa
Václav Blažek <vaclav.blazek@citationtech.net>
2012-10-19 11:37:27 +0000

    shared/dbglog/test: transformed to unit-test
    git-svn-id: https://scm.citationtech.net/trunk/shared/dbglog/dbglog@2282 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- dbglog/test/dbglog.cpp
+++ dbglog/test/dbglog.cpp
@@ -13,23 +13,22 @@ void once() {
 
 BOOST_AUTO_TEST_CASE(dbglog_once)
 {
+    // TODO: test once
     once();
     once();
     once();
 }
 
-
 BOOST_AUTO_TEST_CASE(dbglog_mask)
 {
-    std::string def;
-    while (std::cin >> def) {
-        try {
-            dbglog::mask mask(def);
-            std::cout << def <<  " -> " << mask
-                      << "(" << mask.get() << ")" << std::endl;
-            dbglog::set_mask(mask);
-        } catch (const std::runtime_error &e) {
-            std::cout << def <<  " -> error: " << e.what() << std::endl;
-        }
+    // TODO: test different masks
+    std::string def("ALL");
+    try {
+        dbglog::mask mask(def);
+        std::cout << def <<  " -> " << mask
+                  << "(" << mask.get() << ")" << std::endl;
+        dbglog::set_mask(mask);
+    } catch (const std::runtime_error &e) {
+        std::cout << def <<  " -> error: " << e.what() << std::endl;
     }
 }
